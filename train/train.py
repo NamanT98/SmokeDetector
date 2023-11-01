c4e75067238ca9b600c196a482e4bdbbb37294d0
namant <namant@iitbhilai.ac.in>
2023-11-01 22:32:26 +0530

    made environment

---
+++ train/train.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+print("Imported YOLO Successfully")
+
+model=YOLO("yolov8m.pt")
+
+print("Created Model Successfully")
+
+model.train(data="../Data/data.yaml",epochs=100,optimizer="adam")
